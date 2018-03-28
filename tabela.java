c9b0c45f7c9df7a85d8178865dca9e607dda18e3
Kleber Vales <ksd.campos@gmail.com>
2018-03-28 10:57:38 -0300

    adiçao do jtable

--- tabela.java
+++ tabela.java
@@ -1,26 +1,48 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 
-class Tab2 extends JFrame {
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.Panel;
 
-	JTable jt;
+class Tab2 extends JFrame {
+	
+	private JPanel p;
+	private JFrame f;
+	private JTable jt;
 
 	String[] coluna = { "time", "pontos", "gol" };
-	String[][] linha = { { "homem", "65", "40" }, { "chelsea", "63", "38" } };
+	String[][] linha = { { "Sao Paulo", "65", "40" },{ "homem", "65", "40" }, { "chelsea", "63", "38" } };
 
 	public Tab2() {
 
+		
 	
-
 		jt = new JTable(linha, coluna);
-		jt.setBounds(50, 50, 200, 230);
-		JScrollPane js = new JScrollPane(jt);
-		this.add(js);
-		this.setSize(300, 400);
-		this.setVisible(true);
+		jt.setBounds(100, 100, 200, 300);
+		
+		jt.setRowHeight(50);
+		
+		p = new JPanel(new GridBagLayout());
+		
+		p.add(jt);
+		
+
+		f = new JFrame("Creativity tuts");
+		f.setVisible(true);
+		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		f.add(p);
+		
+		
+		
+	
+	
 	
 
 	}
@@ -29,6 +51,7 @@ class Tab2 extends JFrame {
 
 	public static void main(String[] args) {
 		Tab2 tab2 = new Tab2();
+		
 		
 		
 	}
